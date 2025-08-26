390b598a1179d4a800341cc2fe19b316eeb7914f
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-08-26 14:49:12 +0600

    Optimized UI for small screens and improved layout flexibility

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ function id(id){
     return document.getElementById(id);
 }
 
-//Event Delegation
+//Event Delegation(Way-1)
 id('product-box').addEventListener('click', function(e){
     if(e.target.className.includes('card-btn')){
         const cardBtn = e.target;
@@ -11,7 +11,7 @@ id('product-box').addEventListener('click', function(e){
         const productTitle = cardBtn.parentNode.parentNode.children[1].children[1].children[0].innerText;
         const productPrice = cardBtn.parentNode.parentNode.children[1].children[1].children[1].children[0].innerText;
 
-        //Calculation
+        // Calculation
             const initialPrice = id('Initial-Price').innerText;
             const total = Number(initialPrice) + Number(productPrice);
             id('Initial-Price').innerText = total.toFixed(2);
@@ -44,36 +44,38 @@ document.getElementById('Clear-btn').addEventListener('click', function(){
 })
 
 
-//Traverse Technique
-// const cardBtns = document.getElementsByClassName('card-btn');
-    // for(const cardBtn of cardBtns){
-    //     cardBtn.addEventListener('click',function(){
-            // const productImg = cardBtn.parentNode.parentNode.children[0].children[0].src;
-            // const productTitle = cardBtn.parentNode.parentNode.children[1].children[1].children[0].innerText;
-            // const productPrice = cardBtn.parentNode.parentNode.children[1].children[1].children[1].children[0].innerText;
 
-            //Calculation
+
+//Traverse Technique(Way-2)
+// const cardBtns = document.getElementsByClassName('card-btn');
+//     for(const cardBtn of cardBtns){
+//         cardBtn.addEventListener('click',function(){
+//             const productImg = cardBtn.parentNode.parentNode.children[0].children[0].src;
+//             const productTitle = cardBtn.parentNode.parentNode.children[1].children[1].children[0].innerText;
+//             const productPrice = cardBtn.parentNode.parentNode.children[1].children[1].children[1].children[0].innerText;
+
+            // // Calculation
             // const initialPrice = id('Initial-Price').innerText;
             // const total = Number(initialPrice) + Number(productPrice);
             // id('Initial-Price').innerText = total.toFixed(2);
 
             // Get Parent COntainer
-//             const CardContainer = id('Add-Card-Container');
-//             const createCard = document.createElement('div')
-//             createCard.innerHTML = `
-//                 <div class="bg-gray-200 rounded-lg flex items-center justify-between p-4 mb-4">
-//                     <div class=""><img src="${productImg}" class="w-15 "></div>
-//                     <div>
-//                         <h2 class="text-xl font-semibold">${productTitle}</h2>
-//                         <p class="text-[#797075]">${productPrice}TK</p>
-//                     </div>
-//                 </div>`;
-//             CardContainer.appendChild(createCard);
+            // const CardContainer = id('Add-Card-Container');
+            // const createCard = document.createElement('div')
+            // createCard.innerHTML = `
+            //     <div class="bg-gray-200 rounded-lg flex items-center justify-between p-4 mb-4 gap-8">
+            //         <div class=""><img src="${productImg}" class="w-15 "></div>
+            //         <div>
+            //             <h2 class="text-[1rem] font-semibold">${productTitle}</h2>
+            //             <p class="text-[0.85rem] text-[#797075]">${productPrice}TK</p>
+            //         </div>
+            //     </div>`;
+            // CardContainer.appendChild(createCard);
 
-            // // Quantity Calculation
-            // const quantity = id('Quantity').innerText;
-            // const currentQuantity = Number(quantity) +1 ;
-            // id('Quantity').innerText = currentQuantity;
+            // Quantity Calculation
+//             const quantity = id('Quantity').innerText;
+//             const currentQuantity = Number(quantity) +1 ;
+//             id('Quantity').innerText = currentQuantity;
 //         })
 // }
 
